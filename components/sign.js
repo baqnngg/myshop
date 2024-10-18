954d265edc861b082f96650f2656bc4f44701a21
baqnngg <il8696546@gmail.com>
2024-10-18 15:27:05 +0900

    10_18

--- components/sign.js
+++ components/sign.js
@@ -1,29 +1,92 @@
 import { TextField, Fade } from "@mui/material"
 import styles from "./sign.module.css"
-import { useState } from "react"
+import { use, useState } from "react"
 
 export default function sign(){
     const [idValue, setIdValue] = useState("");
+    const [pwValue, setpwValue] = useState("");
     const [showPwField, setShowPwField] = useState(false);
+    const [showPwCheckField, setshowPwCheckField] = useState(false);
+    const [pwCheckValue, setPwCheckValue] = useState("");
+    const [helpText, setHelpText] = useState("6-12자 이내 영문, 숫자 사용");
+    const [pwCheck, setPwCheck] = useState("8-16자 이내 영문, 숫자, 특수문자 사용가능");
+
+
+    const [falseV, setfalseV] = useState(true);  //에러인지 아닌지
+    const [idError, setidError] = useState(false);
+    const [pwCheckHelpText, setpwCheckHelpText] = useState("");
+    const [pwHelpText, setpwHelpText] = useState("")
+    const [pwError, setpwError] = useState(false);
+    const [pwCheckError, setpwCheckError] = useState(false);
+
     const handleIdChange = (event) => {
         const value = event.target.value;
         setIdValue(value);
 
-        if (value.length > 5){setShowPwField(true);}
-        else{ setShowPwField(false); }
+        const regexId = /^[a-zA-Z0-9]{6,12}$/;
+        if (regexId.test(value)){
+            setHelpText("");
+            setShowPwField(true);
+            setfalseV(true);
+        }else if(value.length > 5 && !regexId.test(value)){
+            setHelpText("ID를 다시입력하세요");
+            setfalseV(false);
+            setShowPwField(false);
+        }
+        else{
+            setHelpText("6-12자 이내 영문, 숫자 사용");
+            setShowPwField(false);  
+            setfalseV(true);
+        }
 
-    }
+        // if (value.length > 5){setShowPwField(true);}
+        // else{ setShowPwField(false); }
+
+    };
+
+    const handlePwChange = (event) => {
+        const value = event.target.value;
+        setpwValue(value);
+        const regexPw = /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
+
+        pwCheck
+        if (regexPw.test(value)){
+            setshowPwCheckField(true);
+            setpwError(false);
+            setpwHelpText("사용 가능한 PW 입니다.")
+            setPwCheck("")
+        } else {
+            setshowPwCheckField(false);
+            setpwError(true);
+            setPwCheck("8-16자 이내 영문, 숫자, 특수문자 사용가능")
+            setpwHelpText("8-16자 이내 영문, 숫자, 특수문자 사용가능")
+        }
+    };
+
+    const handlePwCheckChange = (event) => {
+        const value = event.target.value;
+        setPwCheckValue(value);
+        console.log(pwValue,value)
+        if (pwValue === value) {
+            setpwCheckError(false);
+            setpwCheckHelpText("");
+        } else {
+            setpwCheckError(true);
+            setpwCheckHelpText("비밀번호가 틀렸습니다.");
+        }
+    };
 
     return(
         <>
             <div className={styles.wrapper}>
                 <h2 className={styles.head}>회원가입</h2>
                 <TextField
+                    error = {!falseV}
                     id="id-required"
                     label="ID"
                     // placeholder="ID를 적으세요"
                     placeholder="6-12자 이내 영문, 숫자"
-                    // helperText="6-12자 이내 영문, 숫자 사용가능"
+                    helperText={helpText}
                     value={idValue}
                     onChange={handleIdChange}
                     style={{ width: "250px" , marginBottom:"20px"}}
@@ -31,18 +94,36 @@ export default function sign(){
                         style: { height: "54px" } 
                     }}
                 ></TextField>
+                <TextField
+                    id="password-required"
+                    label="password"
+                    type="password"
+                    placeholder="password를 적으세요"
+                    helperText={pwCheck}
+                    style={{ width: "250px", marginBottom:"20px"}}
+                    InputProps={{
+                        style: { height: "54px"} 
+                    }}
+                    value={pwValue}
+                    onChange={handlePwChange}
+                    fullWidth
+                ></TextField>
                 {showPwField && (
-                    <Fade in={showPwField} timeout={500}>
+                    <Fade in={showPwCheckField} timeout={500}>
                         <TextField
+                        error={pwCheckError}
                             id="password-required"
-                            label="password"
+                            label="비밀번호 확인"
                             type="password"
                             placeholder="password를 적으세요"
-                            helperText="8-16자 이내 영문, 숫자, 특수문자 사용가능"
+                            helperText={pwCheckHelpText}
                             style={{ width: "250px" }}
                             InputProps={{
                                 style: { height: "54px" } 
                             }}
+                            value={pwCheckValue}
+                            onChange={handlePwCheckChange}
+                            fullWidth
                         ></TextField>
                     </Fade>
                 )}
