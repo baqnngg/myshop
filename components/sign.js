68ef080bba9f57f6245fa7a05d9eb1db7c8064ab
baqnngg <il8696546@gmail.com>
2024-10-18 15:31:20 +0900

    10_18

--- components/sign.js
+++ components/sign.js
@@ -1,8 +1,8 @@
-import { TextField, Fade } from "@mui/material"
-import styles from "./sign.module.css"
-import { use, useState } from "react"
+import { TextField, Fade } from "@mui/material";
+import styles from "./sign.module.css";
+import { useState } from "react";
 
-export default function sign(){
+export default function Sign() {
     const [idValue, setIdValue] = useState("");
     const [pwValue, setpwValue] = useState("");
     const [showPwField, setShowPwField] = useState(false);
@@ -11,81 +11,83 @@ export default function sign(){
     const [helpText, setHelpText] = useState("6-12자 이내 영문, 숫자 사용");
     const [pwCheck, setPwCheck] = useState("8-16자 이내 영문, 숫자, 특수문자 사용가능");
 
-    const [falseV, setfalseV] = useState(true);  //에러인지 아닌지
-    const [idError, setidError] = useState(false);
+    const [falseV, setfalseV] = useState(true); // 에러 여부
     const [pwCheckHelpText, setpwCheckHelpText] = useState("");
-    const [pwHelpText, setpwHelpText] = useState("")
+    const [pwHelpText, setpwHelpText] = useState("");
     const [pwError, setpwError] = useState(false);
     const [pwCheckError, setpwCheckError] = useState(false);
+
+    // 학번과 이름 텍스트 강조를 위한 상태
+    const [highlightInfo, setHighlightInfo] = useState(false);
 
     const handleIdChange = (event) => {
         const value = event.target.value;
         setIdValue(value);
 
         const regexId = /^[a-zA-Z0-9]{6,12}$/;
-        if (regexId.test(value)){
+        if (regexId.test(value)) {
             setHelpText("");
             setShowPwField(true);
             setfalseV(true);
-        }else if(value.length > 5 && !regexId.test(value)){
+        } else if (value.length > 5 && !regexId.test(value)) {
             setHelpText("ID를 다시입력하세요");
             setfalseV(false);
             setShowPwField(false);
-        }
-        else{
+        } else {
             setHelpText("6-12자 이내 영문, 숫자 사용");
-            setShowPwField(false);  
+            setShowPwField(false);
             setfalseV(true);
         }
-
     };
 
     const handlePwChange = (event) => {
         const value = event.target.value;
         setpwValue(value);
-        const regexPw = /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
+        const regexPw = /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
 
-        if (regexPw.test(value)){
+        if (regexPw.test(value)) {
             setshowPwCheckField(true);
             setpwError(false);
-            setpwHelpText("사용 가능한 PW 입니다.")
-            setPwCheck("")
+            setpwHelpText("사용 가능한 PW 입니다.");
+            setPwCheck("");
         } else {
             setshowPwCheckField(false);
             setpwError(true);
-            setPwCheck("8-16자 이내 영문, 숫자, 특수문자 사용가능")
-            setpwHelpText("8-16자 이내 영문, 숫자, 특수문자 사용가능")
+            setPwCheck("8-16자 이내 영문, 숫자, 특수문자 사용가능");
+            setpwHelpText("8-16자 이내 영문, 숫자, 특수문자 사용가능");
         }
     };
 
     const handlePwCheckChange = (event) => {
         const value = event.target.value;
         setPwCheckValue(value);
-        console.log(pwValue,value)
+
         if (pwValue === value) {
             setpwCheckError(false);
             setpwCheckHelpText("");
+            setHighlightInfo(true);  // 비밀번호가 맞으면 텍스트 강조
         } else {
             setpwCheckError(true);
             setpwCheckHelpText("비밀번호가 틀렸습니다.");
+            setHighlightInfo(false);  // 비밀번호가 틀리면 텍스트 강조 해제
         }
     };
 
-    return(
+    return (
         <>
             <div className={styles.wrapper}>
                 <h2 className={styles.head}>회원가입</h2>
                 <TextField
-                    error = {!falseV}
+                    error={!falseV}
                     id="id-required"
                     label="ID"
                     placeholder="6-12자 이내 영문, 숫자"
                     helperText={helpText}
                     value={idValue}
                     onChange={handleIdChange}
-                    style={{ width: "250px" , marginBottom:"20px"}}
+                    style={{ width: "250px", marginBottom: "20px" }}
                     InputProps={{
-                        style: { height: "54px" } 
+                        style: { height: "54px" },
                     }}
                 ></TextField>
                 <TextField
@@ -94,9 +96,9 @@ export default function sign(){
                     type="password"
                     placeholder="password를 적으세요"
                     helperText={pwCheck}
-                    style={{ width: "250px", marginBottom:"20px"}}
+                    style={{ width: "250px", marginBottom: "20px" }}
                     InputProps={{
-                        style: { height: "54px"} 
+                        style: { height: "54px" },
                     }}
                     value={pwValue}
                     onChange={handlePwChange}
@@ -105,7 +107,7 @@ export default function sign(){
                 {showPwField && (
                     <Fade in={showPwCheckField} timeout={500}>
                         <TextField
-                        error={pwCheckError}
+                            error={pwCheckError}
                             id="password-required"
                             label="비밀번호 확인"
                             type="password"
@@ -113,7 +115,7 @@ export default function sign(){
                             helperText={pwCheckHelpText}
                             style={{ width: "250px" }}
                             InputProps={{
-                                style: { height: "54px" } 
+                                style: { height: "54px" },
                             }}
                             value={pwCheckValue}
                             onChange={handlePwCheckChange}
@@ -122,10 +124,10 @@ export default function sign(){
                     </Fade>
                 )}
                 <div style={{ marginTop: "20px" }}>
-                    <p>학번: 2514</p>
-                    <p>이름: 장병준</p>
+                    <p className={highlightInfo ? styles.highlight : ""}>학번: 2514</p>
+                    <p className={highlightInfo ? styles.highlight : ""}>이름: 장병준</p>
                 </div>
             </div>
         </>
-    )
+    );
 }
